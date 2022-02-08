1655a74f2a941a267575b6058b378a2aa360ff1a
Felix <90440279+Kubenew@users.noreply.github.com>
2022-02-08 07:44:27 +0100

    Create anticrypto.sh

---
+++ anticrypto.sh
@@ -0,0 +1,12 @@
+Stopping a Struts exploit
+After declaring a VOLUME for /usr/local/tomcat,
+and opting-in for a read-only root FS:
+$ ./2-exploit.sh
+Forwarding traffic to Struts-vulnerable deployment... 
+
+Using Struts to try to download and run a cryptominer... 
+
+/miner.tgz: 
+Read-only file system Processes running:
+PID COMMAND 
+1 java
